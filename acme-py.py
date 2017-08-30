1db4c53e8b5c5c32914515c166a2c1f0b9b2875c
Ashley Lavery <ashley@sa-irc.com>
2017-08-30 19:35:17 +0100

    Added some command line argument parsing

--- acme-py.py
+++ acme-py.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
+
+import argparse
 import sys
 
 def main(argv):
-    print("Running")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default=None, help="Configuration file")
+    parser.add_argument("--acme-dir", default=None, help="Path to the ACME challenge directory")
+
+    challenges = parser.add_mutually_exclusive_group(required=True)
+    challenges.add_argument("--http", action="store_true", help="Use HTTP challenge")
+    #challenges.add_argument("--tls", action="store_true", help="Use TLS challenge")
+    challenges.add_argument("--dns", action="store_true", help="Use DNS challenge")
+
+    args = parser.parse_args(argv)
+
+    if args.http and not args.acme_dir:
+        parser.error("--acme-dir is required for the HTTP challenge")
 
 if __name__ == "__main__": # pragma: no cover
     main(sys.argv[1:])
